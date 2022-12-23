a6c0487d1b1d5ea6f80797ddccba03f1d1669803
Nataliia Levak <nataliialevak@gmail.com>
2022-12-24 02:30:23 +0300

    Add files via upload

---
+++ Lesson01task3.java
@@ -0,0 +1,5 @@
+public class Lesson01task3 {
+    public static void main(String... args) {
+        System.out.println("Java: version 11.");
+    }
+}
---
+++ Lesson01task4.java
@@ -0,0 +1,6 @@
+public class Lesson01task4 {
+    public static void main(String... args) {
+        System.out.println("Java: version 11.");
+        System.out.println("JVM: HotSpot.");
+    }
+}
